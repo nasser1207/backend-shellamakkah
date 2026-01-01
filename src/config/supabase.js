55fc9973ff8b94276c6d75a4924b778bf2cfe967
sheraz4196 <sheraztoor321@gmail.com>
2026-01-01 17:33:59 +0500

    FIX: PERSIST TOKEN REFRESH

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -4,6 +4,6 @@ export const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY,
   {
-    auth: { persistSession: false },
+    auth: { persistSession: false, autoRefreshToken: false },
   }
 );
